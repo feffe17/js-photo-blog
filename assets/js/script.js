82ed6e2438991d9183d924b1bee6ef903d9c394a
feffe1702 <fpm.asd@gmail.com>
2024-10-18 13:06:52 +0200

    [WIP] generate cards from axios

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let rowEl = document.getElementById("row")
+
+
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+.then(response => {
+    // let cardGenerata;
+    console.log(response);
+    response.data.forEach(element => {
+        let urlImg = element.url;
+        let imgDesc = element.title
+        rowEl.innerHTML += `<div class="p-3">
+                    <div class="col bg-white p-3 card d-flex flex-column align-items-center">
+                        <div class="pin">
+                            <img src="./assets/img/pin.svg" width="80%" height="80%" alt="">
+                        </div>
+                        <img src="${urlImg}">
+                        <div class="mt-3">"${imgDesc}"</div>
+                    </div>
+                </div>`
+    });
+    
+})
